50ab2d15fbc9ea4214ed6ab2fd6bf02156d987ca
yeoyinghao <yeo.yinghao@outlook.com>
2026-05-10 17:29:55 +0900

    fix format

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  
+
   tags = {
     Name = "etracker-vpc"
   }
@@ -31,7 +31,7 @@ resource "aws_internet_gateway" "main" {
 # Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
