d30a710dc746b6dda2c2985d8964597a91a6ee94
yeoyinghao <yeo.yinghao@outlook.com>
2026-05-10 17:27:08 +0900

    tf create vpc with sg

--- terraform/s3.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "test-bucket" {
-  bucket = "yinghao-bucket-from-tf-test"
-}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+locals {
+  app_ecr_repository_name = "expense-tracker-api-repo"
+
+  image_tag = "releasing"
+
+  container_port = 8080
+}
