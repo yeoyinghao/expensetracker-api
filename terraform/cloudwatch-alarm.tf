d858c916508297ec774b4d94fd2f0c69a6329630
yeoyinghao <yeo.yinghao@outlook.com>
2026-07-17 09:15:54 +0900

    terraform format

--- terraform/cloudwatch-alarm.tf
+++ terraform/cloudwatch-alarm.tf
@@ -1,12 +1,12 @@
 resource "aws_cloudwatch_metric_alarm" "cpu" {
-  alarm_name                = "et-ecs-cpu"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = 2
-  metric_name               = "CPUUtilization"
-  namespace                 = "AWS/ECS"
-  period                    = 120
-  statistic                 = "Average"
-  threshold                 = 80
-  alarm_description         = "This metric monitors ec2 cpu utilization"
-  alarm_actions             = [aws_sns_topic.cloudwatch_alarm.arn]
+  alarm_name          = "et-ecs-cpu"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 80
+  alarm_description   = "This metric monitors ec2 cpu utilization"
+  alarm_actions       = [aws_sns_topic.cloudwatch_alarm.arn]
 }
